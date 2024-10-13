15ef3212133f69d447ebd70a8e05c082224c1940
Jun Wu Wang <wng.mattia@gmail.com>
2024-10-13 14:32:43 +0200

    Adapt edge inference algorithm to Edge Impulse models

--- src/commons.py
+++ src/commons.py
@@ -3,3 +3,6 @@ class OffloadingDataFiles:
     data_file_path_edge: str = "../edge_inference_times.json"
     data_file_path_sizes: str = "../layer_sizes.json"
     evaluation_file_path: str = "../evaluations/evaluations.csv"
+
+class InputDataFiles:
+    input_data_file_path = "../models/test/test_model/pred_data/pred_data_1.png"
