c211e6f326cf543fe5175c99465c2308ba2bb9e3
Jun Wu Wang <wng.mattia@gmail.com>
2024-10-14 00:22:05 +0200

    Split model_build_split.py in two separate tools and adapt splitting algorithm to take shared layers into account

--- src/models/model_split.py
+++ src/models/model_split.py
@@ -10,7 +10,7 @@ from model_manager_config import ModelManagerConfig
 
 def init_folders(root_folder: str) -> None:
     os.makedirs(f"{root_folder}/layers/", exist_ok=True)
-    os.makedirs(f"{root_folder}/layers/keras/", exist_ok=True)
+    os.makedirs(f"{root_folder}/layers/h5/", exist_ok=True)
     os.makedirs(f"{root_folder}/layers/tflite/", exist_ok=True)
     os.makedirs(f"{root_folder}/layers/h/", exist_ok=True)
 
@@ -23,43 +23,36 @@ def to_tflite(keras_model: Model, save: bool, save_dir: str, name: str) -> bytes
             f.write(tflite_model)
     return tflite_model
 
-
-def save_keras(name: str, model: Model, dir_path: str) -> None:
-    model.save(f'{dir_path}/{name}.keras')
-
-
 def load_keras(name: str, dir_path: str) -> Model:
-    return tf.keras.models.load_model(f'{dir_path}/{name}.keras')
+    return tf.keras.models.load_model(f'{dir_path}/{name}.h5')
 
 
 def create_keras_submodels(save_dir: str, model: Model) -> dict:
     submodels = {}
-    # start with the input tensor
-    input_tensor = Input(shape=model.input_shape[1:])
-    x = input_tensor
+
+    start_layer_index = 0
     # iterate through layers and create submodels
     for i, layer in enumerate(model.layers):
+        if isinstance(layer, layers.InputLayer):
+            start_layer_index = 1
+            continue
+
+        # Create input tensor
+        if isinstance(layer.input_shape, list):
+            input_tensor = []
+            for shape in layer.input_shape:
+                input_tensor.append(Input(shape=shape[1:]))
+        else:
+            input_tensor = Input(shape=layer.input_shape[1:])
+
         # pass the input tensor through each layer sequentially
-        x = layer(x)
+        output_tensor = layer(input_tensor)
         # create the submodel from the input tensor to the current layer's output
-        submodel = Model(inputs=input_tensor, outputs=x)
+        submodel = Model(inputs=input_tensor, outputs=output_tensor)
         submodels[layer.name] = submodel
         # save each submodel to a file
-        submodel.save(f'{save_dir}/submodel_{i}.keras')
+        submodel.save(f'{save_dir}/submodel_{i-start_layer_index}.h5')
     return submodels
-
-
-def build_resnet_from_scratch(img_height=10, img_width=10, num_classes=5) -> Model:
-    # inputs = layers.Input(shape=(img_height, img_width, 3))
-    resnet_model = tf.keras.models.Sequential()
-    # initial Conv Layer
-    resnet_model.add(
-        layers.Conv2D(64, kernel_size=7, strides=2, padding='same', input_shape=(img_height, img_width, 3)))
-    resnet_model.add(layers.BatchNormalization())
-    resnet_model.add(layers.ReLU())
-    resnet_model.add(layers.MaxPooling2D(pool_size=3, strides=2, padding='same'))
-    resnet_model.add(layers.Dense(num_classes, activation='softmax'))
-    return resnet_model
 
 
 if __name__ == "__main__":
@@ -69,17 +62,12 @@ if __name__ == "__main__":
     print(f"main_folder: {main_folder}")
     init_folders(main_folder)
 
-    # build 'keras' model and store it
-    print("building keras model ...")
-    model = build_resnet_from_scratch(img_height=ModelManagerConfig.IMAGE_SIZE, img_width=ModelManagerConfig.IMAGE_SIZE)
-    save_keras(name="resnet_model", model=model, dir_path=main_folder)
-
     # load the model
-    model = load_keras(name="resnet_model", dir_path=main_folder)
+    model = load_keras(name="test_model", dir_path=main_folder)
 
     # creates and save submodels '.tflite'
     print("creating submodels ...")
-    submodels = create_keras_submodels(model=model, save_dir=f"{main_folder}/layers/keras")
+    submodels = create_keras_submodels(model=model, save_dir=f"{main_folder}/layers/h5")
 
     # creates and save submodels '.h'
     for layer_index, item in enumerate(submodels.items()):
@@ -97,8 +85,6 @@ if __name__ == "__main__":
         with open(f'{main_folder}/layers/h/layer_{layer_index}.h', 'w') as header_file:
             header_file.write('#pragma once\n\n')
             header_file.write('#include <cstdint>\n\n')
-            header_file.write('const uint8_t layer_' + str(layer_index - 1) + '[] = {\n')
+            header_file.write('const uint8_t layer_' + str(layer_index) + '[] = {\n')
             header_file.write(model_array)
             header_file.write('\n};\n')
-
-    print(model.summary())
